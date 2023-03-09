7381ec0fcd5f877718ee91ef0d9cbdc10398e50c
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-03-09 16:16:55 +0100

    add class to sub btn + js read value of user

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,16 @@ let kmInput;
 let age;
 
 const kmPrice = 0.21;
+
+//seleziono il bottone del submit
+const btnEl = document.querySelector('button.sub');
+
+// al click creo una funzione che mi legge cosa ha inserito l'utente 
+btnEl.addEventListener('click', function(){
+    console.log('clicked');
+    
+    // leggo i valori inseriti dall'utente
+    const nameEl = document.getElementById("nameInput").value;
+    const kmEl = document.getElementById("kmInput").value;
+    console.log(nameEl, kmEl); 
+})
